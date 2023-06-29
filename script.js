09a6a07404c57abfa73a0dd08528bee1243f8575
César <cesarsantossouza99@gmail.com>
2023-06-29 19:47:30 -0300

    js

---
+++ script.js
@@ -0,0 +1,16 @@
+// const imgs = document.getElementById('img')
+// const img = document.querySelectorAll('#img .carrosel-item')
+
+// let trocaImg = 0
+
+// function carrosel() {
+//     trocaImg++
+
+//     if(trocaImg > img.length -1 ) {
+//         trocaImg = 0
+//     }
+
+//     imgs.style.transform = `translateX(${-trocaImg * 100}%)`
+// }
+
+// setInterval(carrosel, 5000);
